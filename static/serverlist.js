70f24dc0cb7a869daa77ff547a27bba40abf4cd8
noodleWrecker7 <hodgkinson.adam@gmail.com>
2019-07-13 21:54:51 +0100

    i think its got it

--- static/serverlist.js
+++ static/serverlist.js
@@ -8,9 +8,8 @@
 /******************************************************************************
  * Copyright (c) 2019.                                                        *
  * Developed by Adam Hodgkinson                                               *
- * Last modified 13/07/19 21:50                                               *
+ * Last modified 13/07/19 21:54                                               *
  ******************************************************************************/
-
 
 let currentSessionID = getCookie("currentSessionID");
 if (currentSessionID == "") {
